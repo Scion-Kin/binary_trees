49c5f73ada83be99e61891202995158522c0c07a
Scion-Kin <dinturner17@gmail.com>
2024-01-30 19:22:44 +0200

    Task completed

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - traverses a tree in post-order mode
+ * @tree: the binary tree to traverse
+ * @func: the function to call for each node encountered
+ * Return: Nothing
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
