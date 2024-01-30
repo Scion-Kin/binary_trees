05f24788e6c51d4c3fd8d5a0b89e1b37c7fb42ba
Scion-Kin <dinturner17@gmail.com>
2024-01-30 20:43:19 +0200

    Task completed

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the tree to measure
+ * Return: the size of the tree, or 0 if failure
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	const binary_tree_t *current = tree;
+	size_t count = 0;
+
+	if (!tree)
+		return (0);
+
+	while (current->left || current->right)
+	{
+		if (current->left && (current->left->left || current->left->right))
+		{
+			count++;
+			current = current->left;
+		}
+
+		else if (current->right && (current->right->left || current->right->right))
+		{
+			count++;
+			current = current->right;
+		}
+		else if (current->right)
+		{
+			count++;
+			current = current->right;
+		}
+		else if (current->left)
+		{
+			count++;
+			current = current->left;
+		}
+		else
+			return (0);
+	}
+
+	return (count);
+}
