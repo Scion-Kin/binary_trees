5f9b4268234a379bd98ee0e234a50887671fa961
Scion-Kin <dinturner17@gmail.com>
2024-01-30 21:54:32 +0200

    Task completed

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-static size_t count_nodes(const binary_tree_t *tree);
+size_t count_nodes(const binary_tree_t *tree);
 
 /**
  * binary_tree_size - finds the size of a binary tree
@@ -22,15 +22,13 @@ size_t binary_tree_size(const binary_tree_t *tree)
  * Return: the number of nodes in the tree
  */
 
-static size_t count_nodes(const binary_tree_t *tree)
+size_t count_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
 	/* Recursively count nodes in the left and right subtrees */
 	/* These comments are for future reference... or should I say present? */
-	size_t left_count = count_nodes(tree->left);
-	size_t right_count = count_nodes(tree->right);
 
-	return (1 + left_count + right_count);
+	return (1 + count_nodes(tree->left) + count_nodes(tree->right));
 }
