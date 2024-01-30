56da722dcf45e6b4688189b71368ec14761d057c
Scion-Kin <dinturner17@gmail.com>
2024-01-30 19:02:21 +0200

    Task completed

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - traverses a tree in preoder mode
+ * @tree: the binary tree to traverse
+ * @func: the function to call for each node encountered
+ * Return: Nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
