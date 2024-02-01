880573a6659d93f975e598ec0ec00db5029cb8f7
Scion-Kin <dinturner17@gmail.com>
2024-02-01 21:10:04 +0200

    Task completed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+int counter(const binary_tree_t *tree);
+
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect i.e:
  * every node has two children and the length is the same.
@@ -9,19 +11,36 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int count1, count2;
+	int count1 = 0, count2 = 0;
 
 	if (!tree)
 		return (0);
 
-	if (!(tree->left && tree->right))
-		return (0);
-
-	count1 = 1 + binary_tree_is_perfect(tree->left);
-	count2 = 1 + binary_tree_is_perfect(tree->right);
+	count1 += counter(tree->left);
+	count2 += counter(tree->right);
 
 	if (count1 != count2)
 		return (0);
 
 	return (1);
+}
+
+/**
+ * counter - checks if a node has 0 or 2 children, recursively
+ * @tree: the node to check
+ * Return: 1 of perfect, 0 if not
+ */
+
+int counter(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (!tree->left || !tree->right)
+		return (0);
+
+	return(counter(tree->left) + counter(tree->right));
 }
