5e5b6d6491885e09f5745dee864a6bb48a2d7a96
Scion-Kin <dinturner17@gmail.com>
2024-02-01 21:53:19 +0200

    Task completed

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_is full - checks if a binary tree is full
+ * @tree: the node/binary tree to test
+ * Return: 1 if full, 0 if not.
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
